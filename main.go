15266592b9734df40dfd9a0d2b589b4f916e52e8
ayushman101 <amansinghayush@gmail.com>
2024-04-17 00:46:39 +0530

    structs added

--- main.go
+++ main.go
@@ -6,7 +6,48 @@ import (
 	"os"
 )
 
+type Driver struct {
+	Id string
+	x  float32
+	y  float32
+}
+
+type Rider struct {
+	Id string
+	x  float32
+	y  float32
+}
+
+type Ride struct {
+	Id          string
+	RiderIndex  int32
+	DriverIndex int32
+	Bill        float32
+}
+
+type Drivers []Driver
+type Riders []Rider
+type Rides []Ride
+
+// returns a slice of Drivers Id under 5km radius in ascending order.
+// If distance is same then sort in lexicographically
+func (d *Drivers) NearestFive(sourceX float32, sourceY float32) ([]string, error) {
+	var Ids []string
+
+	return Ids, nil
+}
+
 func main() {
+
+	var drivers Drivers
+	var riders Riders
+	var rides Rides
+
+	//Just to remove erros whild dev
+	_ = drivers
+	_ = riders
+	_ = rides
+
 	cliArgs := os.Args[1:]
 
 	if len(cliArgs) == 0 {
