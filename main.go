19d0da2e2e3ab9118e859cd1655a4eedfbce1aee
ayushman101 <amansinghayush@gmail.com>
2024-04-17 23:55:55 +0530

    Match rider and print 5 drivers

--- main.go
+++ main.go
@@ -40,23 +40,65 @@ type Rides []Ride
 // If distance is same then sort in lexicographically
 func (d *Drivers) NearestFive(sourceX float32, sourceY float32) []int32 {
 	var Ids []int32
-
+	var distances []float32
 	it := 0
 
 	dri := *d
 
-	for i := 0; i < len(*d); i++ {
-		if it == 5 {
-			break
-		}
+	for i := 0; i < len(dri); i++ {
 
 		dist := math.Sqrt(math.Pow(float64(dri[i].x)-float64(sourceX), 2) + math.Pow(float64(dri[i].y)-float64(sourceY), 2))
 
 		if dist <= 5 {
-			Ids = append(Ids, int32(i))
-			it++
+			if it < 5 {
+				Ids = append(Ids, int32(i))
+				distances = append(distances, float32(dist))
+				it++
+			} else {
+				//If we already have 5 drivers and there is another under 5km
+				//Then we check if it is in top 5 by comparing it with farthest driver among the five
+				maxDist := distances[0]
+				k := 0
+				for j := 1; j < len(Ids); j++ {
+					if distances[j] > maxDist {
+						maxDist = distances[j]
+						_ = k
+						k = j
+					}
+				}
+
+				if dist < float64(maxDist) {
+					distances[k] = float32(dist)
+					Ids[k] = int32(i)
+				}
+			}
 		}
+
 	}
+
+	for i := 1; i < len(Ids); i++ {
+		pos := i
+		id := Ids[i]
+		dis := distances[i]
+		for j := i - 1; j >= 0; j-- {
+			if distances[j] > dis {
+				distances[j+1] = distances[j]
+				Ids[j+1] = Ids[j]
+				pos--
+
+			} else if distances[j] == dis && strings.Compare(dri[Ids[j]].Id, dri[Ids[i]].Id) > 0 {
+				distances[j+1] = distances[j]
+				Ids[j+1] = Ids[j]
+				pos--
+			} else {
+				break
+			}
+		}
+
+		distances[pos] = dis
+		Ids[pos] = id
+	}
+
 	return Ids
 }
 
